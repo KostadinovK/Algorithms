c3fbfe2e23e90de72f18949c564ce09d677df564
KostadinovK <kostadinovk2001@gmail.com>
2019-03-18 22:55:55 +0200

    Solved the first 3 tasks from the Sorting and Searching homework to lecture 01

---
+++ 01-Recursion-Sorting-And-Searching-Algos/Sorting-And-Searching-Homework/04-Words/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<char> letters = Console.ReadLine().ToCharArray().ToList();
+
+        int distinctCount = letters.Distinct().ToList().Count;
+
+
+        Dictionary<char, int> occurrences = new Dictionary<char, int>();
+
+        foreach (var letter in letters)
+        {
+            if (!occurrences.ContainsKey(letter))
+            {
+                occurrences[letter] = 0;
+            }
+
+            occurrences[letter]++;
+        }
+
+        int maxOccurrences = occurrences.Values.Max();
+
+        if (maxOccurrences >= distinctCount)
+        {
+            Console.WriteLine(0);
+        }
+    }
+
+}
