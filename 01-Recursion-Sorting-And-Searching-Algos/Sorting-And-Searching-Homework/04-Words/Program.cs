2315c534cadc61725fa4e452e16137c33d599d11
KostadinovK <kostadinovk2001@gmail.com>
2019-03-19 11:45:54 +0200

    Solved Searching and Sorting homework to lecture 01

--- 01-Recursion-Sorting-And-Searching-Algos/Sorting-And-Searching-Homework/04-Words/Program.cs
+++ 01-Recursion-Sorting-And-Searching-Algos/Sorting-And-Searching-Homework/04-Words/Program.cs
@@ -5,31 +5,83 @@ using System.Linq;
 
 public class Program
 {
+    private static int Count = 0;
+    private static char[] letters;
+
     public static void Main(string[] args)
     {
-        List<char> letters = Console.ReadLine().ToCharArray().ToList();
+        letters = Console.ReadLine().ToCharArray();
 
-        int distinctCount = letters.Distinct().ToList().Count;
-
-
-        Dictionary<char, int> occurrences = new Dictionary<char, int>();
-
-        foreach (var letter in letters)
+        var distinct = letters.Distinct().ToArray();
+        if (distinct.Length == letters.Length)
         {
-            if (!occurrences.ContainsKey(letter))
-            {
-                occurrences[letter] = 0;
-            }
-
-            occurrences[letter]++;
+            Console.WriteLine(GetFact(distinct.Length));
+            return;
         }
 
-        int maxOccurrences = occurrences.Values.Max();
+        GeneratePerm(0);
 
-        if (maxOccurrences >= distinctCount)
+        Console.WriteLine(Count);
+    }
+
+    private static long GetFact(int distinctLength)
+    {
+        long res = 1;
+
+        for (int i = 1; i <= distinctLength; i++)
         {
-            Console.WriteLine(0);
+            res *= i;
+        }
+
+        return res;
+    }
+
+    private static void GeneratePerm(int index)
+    {
+        if (index == letters.Length)
+        {
+            if (IsValidPerm())
+            {
+                Count++;
+            }
+
+            return;
+        }
+
+        HashSet<char> swapped = new HashSet<char>();
+
+        for (int i = index; i < letters.Length; i++)
+        {
+            if (!swapped.Contains(letters[i]))
+            {
+                Swap(index, i);
+                GeneratePerm(index + 1);
+                Swap(index, i);
+
+                swapped.Add(letters[i]);
+            }
+
+            
         }
     }
 
+    private static bool IsValidPerm()
+    {
+        for (int i = 0; i < letters.Length - 1; i++)
+        {
+            if (letters[i] == letters[i + 1])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static void Swap(int index, int index2)
+    {
+        char temp = letters[index];
+        letters[index] = letters[index2];
+        letters[index2] = temp;
+    }
 }
